52c429bdeec322fec83272a4ec8fdf99103c22a5
Gert van Hout <gertvhout@gmail.com>
2022-06-28 13:20:55 +0200

    Added PokemonController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PokemonController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/pokemon', function () {
-    return 'balbasaur';
-});
+Route::get('/pokemon', [PokemonController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
